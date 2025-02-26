965d006ae8aec98dfeda911fad876bc29229761d
LarsMichinruhe <thomas.gerhards@bms-cs.de>
2025-02-26 14:22:58 +0100

    Add files via upload

--- src/main/java/de/bmscs/bwslearning/dto/LoanRequest.java
+++ src/main/java/de/bmscs/bwslearning/dto/LoanRequest.java
@@ -1,40 +1,43 @@
 package de.bmscs.bwslearning.dto;
 
+/*
+ * LoanRequest represents the request for a loan.
+ */
 public class LoanRequest {
-	private double principal;
-	private double annualInterestRate;
-	private int years;
-	private double monthlyPayment;
+    private double principal;
+    private double annualInterestRate;
+    private int years;
+    private double annualRepaymentRate;
 
-	public double getPrincipal() {
-		return principal;
-	}
+    public double getPrincipal() {
+        return principal;
+    }
 
-	public void setPrincipal(double principal) {
-		this.principal = principal;
-	}
+    public void setPrincipal(double principal) {
+        this.principal = principal;
+    }
 
-	public double getAnnualInterestRate() {
-		return annualInterestRate;
-	}
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
 
-	public void setAnnualInterestRate(double annualInterestRate) {
-		this.annualInterestRate = annualInterestRate;
-	}
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
 
-	public int getYears() {
-		return years;
-	}
+    public int getYears() {
+        return years;
+    }
 
-	public void setYears(int years) {
-		this.years = years;
-	}
+    public void setYears(int years) {
+        this.years = years;
+    }
 
-	public double getMonthlyPayment() {
-		return monthlyPayment;
-	}
+    public double getAnnualRepaymentRate() {
+        return annualRepaymentRate;
+    }
 
-	public void setMonthlyPayment(double monthlyPayment) {
-		this.monthlyPayment = monthlyPayment;
-	}
+    public void setAnnualRepaymentRate(double annualRepaymentRate) {
+        this.annualRepaymentRate = annualRepaymentRate;
+    }
 }
